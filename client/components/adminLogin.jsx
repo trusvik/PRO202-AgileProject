ef4964e6615f54b8a681d1865cf3fdea2f5273cf
ConstantLoghead <87537074+ConstantLoghead@users.noreply.github.com>
2024-05-27 13:37:01 +0200

    Intermediate
    Tester commit med noen endringer

--- client/components/adminLogin.jsx
+++ client/components/adminLogin.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-
+import './adminLogin.css'
 
 const AdminLogin = () => {
     const givenUsr = "";
@@ -7,7 +7,17 @@ const AdminLogin = () => {
 
     return (
         <div id="admLogMainContainer">
-            <p></p> 
+            <div className="admFloater">
+                <div className="inputBox">
+                    <input type="text" placeholder="Password..."/>
+                    <button>TEST</button>
+                </div>
+                <div className="inputBox">
+                    <input type="text" placeholder="Username..."/>
+                    <button>TEST</button>
+                </div>
+            </div>
+
         </div>
     )
 }
