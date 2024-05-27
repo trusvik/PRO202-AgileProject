9804f6d6ce166b170b3e4528a78478644844092c
ConstantLoghead <87537074+ConstantLoghead@users.noreply.github.com>
2024-05-27 13:04:43 +0200

    Admin Login Inital Psuh
    La til en jsx component og en css

---
+++ client/components/adminLogin.jsx
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+
+const AdminLogin = () => {
+    const givenUsr = "";
+    const givenPwd = "";
+
+    return (
+        <div id="admLogMainContainer">
+            <p></p> 
+        </div>
+    )
+}
+
+export default AdminLogin;
