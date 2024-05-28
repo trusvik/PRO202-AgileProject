fa8f9ea2eb1a7ac5456a61d052918e414ae703f6
Tobias <toru010@student.kristiania.no>
2024-05-28 12:27:16 +0200

    Fixed frontend to work with api

--- client/components/admin.jsx
+++ client/components/admin.jsx
@@ -1,71 +1,78 @@
+import React, { useEffect, useState } from 'react';
 import './admin.css';
 
-function Admin(){
-    
-    return(
+function Admin() {
+    const [plays, setPlays] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPlays = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/admin/plays/get');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setPlays(data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching plays:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchPlays();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
         <>
-    
             <header id="containerHeader">
-
                 <div id="flexContainerLeft">
-                    <h1 id='logo'>LOADING..</h1>
+                    <h1 id='logo'>Plays</h1>
                 </div>
-
                 <div id="flexContainerRight">
                     <p id='userName'>Admin</p>
                     <img alt="Admin" id='profilePicture'/>
                 </div>
-
             </header>
 
-        
-            <section id='partent-margin'>
-
+            <section id='parent-margin'>
                 <section id='containerSectionButton'>
                     <button id='createNewButton'>Create new</button>
                 </section>
-        
+
                 <section id='containerSectionName'>
                     <div id='start'>
                         <div id='one'><p>Id</p></div>
                         <div id='two'><p>Name</p></div>
-                        <div id='three'><p>Number of questions</p></div>
+                        <div id='three'><p>Number of scenarios</p></div>
                     </div>
                     <div id='end'>
                         <p>Edit</p>
                     </div>
                 </section>
-                
-                <section id='containerSectionName'>
-                    <div id='start'>
-                        <div id='one'><p>1</p></div>
-                        <div id='two'><p>Peer Gynt</p></div>
-                        <div id='three'><p>6</p></div>
-                    </div>
-                    <div id='end'>
-                        <button id='play'>Play</button>
-                        <button id='edit'>Edit</button>
-                        <button id='remove'>Remove</button>
-                    </div>
-                </section>
 
-                <section id='containerSectionName'>
-                    <div id='start'>
-                        <div id='one'><p>2</p></div>
-                        <div id='two'><p>Peer Gynt og den forsvunende diamnt</p></div>
-                        <div id='three'><p>6</p></div>
-                    </div>
-                    <div id='end'>
-                        <button id='play'>Play</button>
-                        <button id='edit'>Edit</button>
-                        <button id='remove'>Remove</button>
-                    </div>
-                </section>
-
+                {plays.map(play => (
+                    <section key={play._id} id='containerSectionName'>
+                        <div id='start'>
+                            <div id='one'><p>{play._id}</p></div>
+                            <div id='two'><p>{play.play}</p></div>
+                            <div id='three'><p>{play.scenarios}</p></div>
+                        </div>
+                        <div id='end'>
+                            <button id='play'>Play</button>
+                            <button id='edit'>Edit</button>
+                            <button id='remove'>Remove</button>
+                        </div>
+                    </section>
+                ))}
             </section>
         </>
     );
-
 }
 
 export default Admin;
