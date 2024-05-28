347cf624bd38812a010f6ca5b58b9adb1c05ecc0
Tobias <toru010@student.kristiania.no>
2024-05-28 15:26:42 +0200

    Remove unnecessary files

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { Link, Route, Routes} from "react-router-dom";
-import FrontPage from "./components/frontPage";
-import ProductPage from "./components/productPage";
-import AboutPage from "./components/aboutPage";
 import AdminLogin from "./components/adminLogin";
 import PinPage from "./components/pinPage"
 import Admin from "./components/admin";
@@ -14,9 +11,6 @@ export function App() {
         <div id={"page_container"}>
             <nav>
                 <h2>
-                    <Link to={`/frontPage/`}>Home</Link>
-                    <Link to={`/productPage/`}>Products</Link>
-                    <Link to={`/aboutPage/`}>About</Link>
                     <Link to={`/adminLogin/`}>Admin Login</Link>
                     <Link to={`/pinPage/`}>Pin Page</Link>
                     <Link to={`/admin/`}>Admin Page</Link>
@@ -24,9 +18,6 @@ export function App() {
                 </h2>
             </nav>
             <Routes>
-                <Route path="/frontPage/" element={<FrontPage />} />
-                <Route path="/productPage/" element={<ProductPage />} />
-                <Route path="/aboutPage/" element={<AboutPage />} />
                 <Route path="/adminLogin/" element={<AdminLogin />} />
                 <Route path="/pinPage/" element={<PinPage/>} />
                 <Route path="/admin/" element={<Admin/>} />
