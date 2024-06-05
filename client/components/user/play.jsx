35894ce05623926b4f5cfa1d1c9ad0415516a0f6
Littledude12 <kevinunited@hotmail.com>
2024-06-05 19:18:14 +0200

    lagt til timer og litt funksjon på knappene

--- client/components/user/play.jsx
+++ client/components/user/play.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './play.css';
 import {useNavigate} from "react-router-dom";
 
@@ -13,12 +13,32 @@ const Play = () => {
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [votes, setVotes] = useState([0, 0, 0, 0]);
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [timeLeft, setTimeLeft] = useState(60);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            
+            setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+        }, 1000); 
+  
+        return () => clearInterval(timer);
+    }, []);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            localStorage.setItem('votes', JSON.stringify(votes));
+            navigate('/resultPage');
+        }
+    }, [timeLeft, votes, navigate]);
+
 
     const handleAnswer = (selectedOptionIndex) => {
         const newVotes = [...votes];
         newVotes[selectedOptionIndex]++;
         setVotes(newVotes);
+        setSelectedOption(selectedOptionIndex);
 
 
         const nextQuestionIndex = currentQuestionIndex + 1;
@@ -26,13 +46,17 @@ const Play = () => {
             setCurrentQuestionIndex(nextQuestionIndex);
         } else {
             localStorage.setItem('votes', JSON.stringify(newVotes));
-            navigate('/resultPage');
+
+            setTimeout(() => {
+                navigate('/resultPage');
+            }, 3000);
         }
     };
 
     return (
         <div className="questionContainer">
-            <h2 className="loadingText">LOADING..</h2>
+            <h1 className="loadingText">LOADING..</h1>
+            <h2 className="timer">Tid igjen: {timeLeft} sekunder</h2>
             <h3 className="questionText">{questions[currentQuestionIndex].question}</h3>
             <div className="optionsContainer">
                 {questions[currentQuestionIndex].options.map((option, index) => (
@@ -45,7 +69,11 @@ const Play = () => {
                     </button>
                 ))}
             </div>
+            {selectedOption !== null && (
+                <p className="selectedOptionText">Du valgte: {questions[currentQuestionIndex].options[selectedOption]}</p>
+            )}
         </div>
+
     );
 
 }
